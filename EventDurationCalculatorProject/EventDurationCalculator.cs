0aad6a515316ed35d0d1a4ef8cd8b0ba555e96b3
Wittner Attila <b1witatt@vasvari.org>
2025-09-04 17:33:45 +0200

    Event Calculator

--- EventDurationCalculatorProject/EventDurationCalculator.cs
+++ EventDurationCalculatorProject/EventDurationCalculator.cs
@@ -4,7 +4,10 @@
     {
         public static (DateOnly, int, int) CalculateEnd(DateOnly startDate, int startHour, int startMinute, int durationHours, int durationMinutes)
         {
-            return (new DateOnly(), 0, 0);
+            if (durationHours < 0) throw new ArgumentException("Az esemény hossza órában nem lehet negatív!", nameof(durationHours));
+            if (durationMinutes < 0) throw new ArgumentException("Az esemény hossza percben nem lehet negatív!", nameof(durationMinutes));
+            DateTime endDateTime = new DateTime(startDate.Year, startDate.Month, startDate.Day, startHour, startMinute, 0).AddHours(durationHours).AddMinutes(durationMinutes);
+            return (new DateOnly(endDateTime.Year, endDateTime.Month, endDateTime.Day), endDateTime.Hour, endDateTime.Minute);
         }
     }
 }
--- SzamjegyekProjekt/Szamjegyek.cs
+++ SzamjegyekProjekt/Szamjegyek.cs
@@ -8,23 +8,11 @@ namespace SzamjegyekProjekt
 {
     internal class Szamjegyek
     {
-        /// <summary>
-        /// Eldönti, hogy a szám legalább kétjegyű-e.
-        /// </summary>
-        /// <param name="szam">A vizsgált szám</param>
-        /// <returns>Igaz, ha a szám két vagy több számjegyből áll, egyébként hamis</returns>
-        /// <example>LegalabbKetjegyu(5) => false, LegalabbKetjegyu(10) => true</example>
         public static bool LegalabbKetjegyu(int szam)
         {
             return Math.Abs(szam).ToString().Length >= 2;
         }
 
-        /// <summary>
-        /// Megfordítja a szám számjegyeit, ha legalább kétjegyű.
-        /// </summary>
-        /// <param name="szam">A vizsgált szám</param>
-        /// <returns>A megfordított szám, vagy eredeti érték, ha egyjegyű</returns>
-        /// <example>ForditottSzam(123) => 321, ForditottSzam(-45) => -54, ForditottSzam(7) => 7</example>
         public static int ForditottSzam(int szam)
         {
             if (!LegalabbKetjegyu(szam)) return szam;
@@ -34,13 +22,6 @@ namespace SzamjegyekProjekt
             return szam < 0 ? -eredmeny : eredmeny;
         }
 
-
-        /// <summary>
-        /// Eldönti, hogy egy szám palindrom-e (tükörszám).
-        /// </summary>
-        /// <param name="szam">A vizsgált szám</param>
-        /// <returns>Igaz, ha a szám palindrom, egyébként hamis</returns>
-        /// <example>PalindromE(121) => true, PalindromE(123) => false</example>
         public static int SzamjegyekOsszege(int szam)
         {
             if (!LegalabbKetjegyu(szam)) return szam;
@@ -48,25 +29,12 @@ namespace SzamjegyekProjekt
             return abs.ToString().Select(c => c - '0').Sum();
         }
 
-        /// <summary>
-        /// Eldönti, hogy egy szám palindrom-e (tükörszám).
-        /// </summary>
-        /// <param name="szam">A vizsgált szám</param>
-        /// <returns>Igaz, ha a szám palindrom, egyébként hamis</returns>
-        /// <example>PalindromE(121) => true, PalindromE(123) => false</example>
         public static bool PalindromE(int szam)
         {
             if (!LegalabbKetjegyu(szam)) return false;
             return Math.Abs(szam) == ForditottSzam(szam);
         }
 
-        /// <summary>
-        /// Visszaadja egy szám digitális gyökét.
-        /// A számjegyek ismételt összege addig, amíg egyjegyű nem lesz.
-        /// </summary>
-        /// <param name="szam">A vizsgált szám</param>
-        /// <returns>A digitális gyök</returns>
-        /// <example>DigitalisGyok(987) => 6, DigitalisGyok(5) => 5</example>
         public static int DigitalisGyok(int szam)
         {
             if (!LegalabbKetjegyu(szam)) return szam;
@@ -78,12 +46,6 @@ namespace SzamjegyekProjekt
             return eredmeny;
         }
 
-        /// <summary>
-        /// Balról jobbra haladva kivonja a szám számjegyeit egymásból.
-        /// </summary>
-        /// <param name="szam">A vizsgált szám</param>
-        /// <returns>A balról jobbra kivonás eredménye, vagy az eredeti szám ha egyjegyű</returns>
-        /// <example>BalrolJobbraKivonas(123) => -4 (1 - 2 - 3)</example>
         public static int BalrolJobbraKivonas(int szam)
         {
             if (!LegalabbKetjegyu(szam)) return szam;
@@ -95,12 +57,6 @@ namespace SzamjegyekProjekt
             return szam < 0 ? -eredmeny : eredmeny;
         }
 
-        /// <summary>
-        /// Megfordítja a számot, majd balról jobbra kivonja annak számjegyeit egymásból.
-        /// </summary>
-        /// <param name="szam">A vizsgált szám</param>
-        /// <returns>A fordított szám számjegyeinek balról-jobbra kivonása</returns>
-        /// <example>BalrolJobbraKivonasForditva(123) => 0 (3 - 2 - 1)</example>
         public static int BalrolJobbraKivonasForditva(int szam)
         {
             return BalrolJobbraKivonas(ForditottSzam(szam));
